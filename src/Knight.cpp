74ba8d6e94ad41612d86537ad97eed9656e59438
mark <41304553+MarkV2323@users.noreply.github.com>
2020-11-30 21:43:57 -0800

    Finished #13, added tests & example vis test in pawn.

--- src/Knight.cpp
+++ src/Knight.cpp
@@ -28,5 +28,63 @@ inline bool basicCheck(Coord testCoord, Board &board, enumTeam team) {
 
 // returns a vector of the possible moves.
 std::vector<Coord> Knight::possibleMoves() {
+    // vector to return.
+    std::vector<Coord> possibleMoves;
+    Board &board = Board::get();
+    enumTeam team = getTeam();
+    Coord currentLocation = this->getLocation();
+    Coord testLocation = currentLocation;
+
+    // Check Northern Locations (Does 2W,1N -> 1W,2N -> 1E,2N -> 2E,1N)
+    testLocation.add(Coord(-2, -1));
+    if (basicCheck(testLocation, board, team)) {
+        possibleMoves.push_back(testLocation);
+    }
+    testLocation = currentLocation;
+    testLocation.add(Coord(-1, -2));
+    if (basicCheck(testLocation, board, team)) {
+        possibleMoves.push_back(testLocation);
+    }
+    testLocation = currentLocation;
+    testLocation.add(Coord(1, -2));
+    if (basicCheck(testLocation, board, team)) {
+        possibleMoves.push_back(testLocation);
+    }
+    testLocation = currentLocation;
+    testLocation.add(Coord(2, -1));
+    if (basicCheck(testLocation, board, team)) {
+        possibleMoves.push_back(testLocation);
+    }
+    testLocation = currentLocation;
+
+    // Check Southern Locations (Does 2W,1S -> 1W,2S -> 1E,2S -> 2E,1S)
+    testLocation.add(Coord(-2, 1));
+    if (basicCheck(testLocation, board, team)) {
+        possibleMoves.push_back(testLocation);
+    }
+    testLocation = currentLocation;
+    testLocation.add(Coord(-1, 2));
+    if (basicCheck(testLocation, board, team)) {
+        possibleMoves.push_back(testLocation);
+    }
+    testLocation = currentLocation;
+    testLocation.add(Coord(1, 2));
+    if (basicCheck(testLocation, board, team)) {
+        possibleMoves.push_back(testLocation);
+    }
+    testLocation = currentLocation;
+    testLocation.add(Coord(2, 1));
+    if (basicCheck(testLocation, board, team)) {
+        possibleMoves.push_back(testLocation);
+    }
+    testLocation = currentLocation;
+
+    // returns an empty vector if no possible moves exist.
+    if (possibleMoves.empty()) {
+        return {};
+    }
+
+    // returns the possibleMoves vector.
+    return possibleMoves;
 
 }
