e784e14a31b5ac37f92ef8fffeb98fbe0b81ca34
mark <41304553+MarkV2323@users.noreply.github.com>
2020-12-02 15:07:39 -0800

    Implemented #15, added tests

---
+++ header/Rook.hpp
@@ -0,0 +1,23 @@
+#ifndef FINALPROJECT_ROOK_HPP
+#define FINALPROJECT_ROOK_HPP
+#include "Piece.hpp"
+
+class Rook : public Piece {
+private:
+
+public:
+    // Constructors
+    Rook() : Piece() { };
+    Rook(enum enumTeam aTeam, Coord aCoord)
+            : Piece("R", 25, aTeam, aCoord){ };
+
+    // Destructor
+    ~Rook() { };
+
+    // Non Virtual Functions
+
+    // Virtual Functions from parent
+    std::vector<Coord> possibleMoves();
+
+};
+#endif //FINALPROJECT_ROOK_HPP
