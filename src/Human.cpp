a87db1e083bfef015fcb11e5bbdf09df36f85232
mark <41304553+MarkV2323@users.noreply.github.com>
2020-12-08 01:26:58 -0800

    Implements #38, #52, #53, can play chess with AI in human tester.

--- src/Human.cpp
+++ src/Human.cpp
@@ -2,29 +2,71 @@
 #include "../header/Board.hpp"
 #include "../header/Game.hpp"
 
+#include <algorithm>
 #include <curses.h>
 #include <iostream>
 #include <cstdlib>
 
+// Global for object.
+bool markedPiece = false;
+Coord markedLocation(-1,-1); // null value for markedLocation is (-1,-1)
+vector<Coord> markedPositions;
+
 // Highlight PossibleMove at current piece Function
 inline void highlight_current_piece(Board &b) {
     // gets current coords, checks if a piece is at these coords.
-    if (b.piece(b.cursor()) != nullptr) {
-        b.highlightedSquares(b.piece(b.cursor())->possibleMoves());
+    if (b.piece(b.cursor()) != nullptr && !markedPiece) {
+        if (b.piece(b.cursor())->possibleMoves().empty()) {
+            b.highlightedSquares({});
+        } else {
+            b.highlightedSquares(b.piece(b.cursor())->possibleMoves());
+        }
     } else {
-        b.highlightedSquares({});
+        if (markedPiece) {
+            // does nothing
+        } else {
+            // clears highlighting
+            b.highlightedSquares({});
+        }
     }
 }
 
 // Cursor Move Function (arrow keys to move, x to exit)
-inline void move_cursor(Board &b) {
+inline void move_cursor(Board &b, Game &g, enum Team team) {
 
     switch (getch()) {
-        case KEY_ENTER:
-            // player has selected a piece to move, will require another
-            // keystroke (enter again) in order to finalize the requested move.
-            // the cursor must be placed at a valid possibleMove highlighted square else it exits.
-            
+        case 'x':
+            // a marked piece exists
+            if (markedPiece) {
+                // checks if the cursor is currently at a valid move.
+                if (std::find(markedPositions.begin(), markedPositions.end(), b.cursor()) != markedPositions.end()) {
+                    // performs move
+                    g.move(Command(markedLocation, b.cursor()));
+                    // resets marking variables
+                    markedPiece = false;
+                    highlight_current_piece(b);
+                    drawTick();
+                } else {
+                    // not at a valid position, will reset the marking variables.
+                    markedPiece = false;
+                    highlight_current_piece(b);
+                    drawTick();
+                }
+            }
+            // a marked piece does not exist.
+            else {
+                // checks if the user is allowed to select this piece.
+                if (b.piece(b.cursor()) != nullptr) {
+                    if (b.piece(b.cursor())->getTeam() == team && !b.piece(b.cursor())->possibleMoves().empty()) {
+                        // the user is allowed to select this piece
+                        // does the marking procedure.
+                        markedPiece = true;
+                        markedLocation = b.cursor();
+                        markedPositions = b.piece(b.cursor())->possibleMoves();
+                    }
+                }
+            }
+            // END OF SELECTED PIECE CASE
             break;
         case KEY_UP:
             b.moveCursor(UP);
@@ -46,7 +88,7 @@ inline void move_cursor(Board &b) {
             highlight_current_piece(b);
             drawTick();
             break;
-        case 'x':
+        case 's':
             drawTick();
             highlight_current_piece(b);
             break;
@@ -69,6 +111,5 @@ void Human::tick(Game &g) {
     // gets the board
     Board &b = Board::get();
     // allows player to move cursor
-    move_cursor(b);
-
+    move_cursor(b, g, this->getTeam());
 }
