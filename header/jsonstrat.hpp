8797ace7d156b7385d135080a4ecb99033cc6f5d
christopherluu-dev <christopherluu.dev@gmail.com>
2020-12-10 17:44:07 -0800

    altered the JSON strat and allows to read from json file now

--- header/jsonstrat.hpp
+++ header/jsonstrat.hpp
@@ -10,10 +10,37 @@ class JSONstrat : public SaveStrategy {
     public:
         void read(vector<Command> &moves){
             ifstream file;
+            string input;
             string filename = "moves.json";
             file.open(filename.c_str());
-            
-        
+            moves.clear();
+            getline(file, input);
+            while(!file.eof()){
+                getline(file, input);
+                char sourcex;
+                char destx;
+                int filesourcex = 0;
+                int filesourcey = atoi(&input[2]);
+                int filedestx = 0;
+                int filedesty = atoi(&input[4]);
+                sourcex = input[1];
+                destx = input[3];
+                
+                for(int i = sourcex; i > 'a'; i--){
+                    filesourcex++;
+                }
+
+                for(int i = destx; i > 'a'; i--){
+                    filedestx++;
+                }
+
+                Coord sourcecoord(filesourcex, filesourcey);
+                Coord destcoord(filedestx, filedesty);
+                Command infromfile(sourcecoord, destcoord);
+                moves.push_back(infromfile);
+            }
+            moves.pop_back();
+            file.close();
         }
 
         void write(vector<Command> &moves) {
